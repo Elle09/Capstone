2955c000b8fcb9554c95325c6b391dd7b4e9d710
Elle09 <133173241+Elle09@users.noreply.github.com>
2023-11-30 14:11:42 -0500

    external api and regex added

--- main.js
+++ main.js
@@ -1,109 +1,100 @@
-
 const circle = document.querySelector(".circle");
 const smCircle = document.querySelector(".small-circle");
 const mdCircle = document.querySelector(".medium-circle");
+const outlineTriangle = document.querySelector(".outlinetriangle");
 const triangle = document.querySelector(".triangle");
 const tbox = document.querySelector("#tbox");
 const btnShakeIt = document.querySelector("#btn-shake-it");
-const readingsP = document.querySelector("#readings");
+const input = document.getElementById('input');
+const inputControl = input.parentElement; // .input-control
 
 
 //brings data from 3rd party api, injects into tbox
-function api()  {
-
-    fetch('https://yesno.wtf/api')
-    .then((data) => {
-        return data.json();
-    })
-    .then((completedata) => {
-        console.log(completedata.answer);
-        //document.getElementById('tbox').innerHTML = completedata.answer;
+function getFromEightBallApi() {
+  fetch("http://localhost:3000/")
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById('tbox').innerHTML = data.body;
     })
     .catch((err) => {
-        console.log('FAILURE');
-    })
-    
-    }
-    
-    //innate responses random injection of elements
-    let fortunes = ['it is certain', 'ask again later', 'very doubtful', 'outlook good','better not tell you now','it is decidedly so','concentrate and ask again', api()];
-    
-    function randomize() {
-    
-       let injectRandom = Math.floor(Math.random() * (fortunes.length));
-       document.getElementById('tbox').innerHTML= fortunes[injectRandom];
-    };
+      console.log("FAILURE");
+    });
+}
 
-//DOESNT WORK
-//setTimeout(randomize, 5000);
-    
 //clears input after user submits text
-    function clearInput() {
-        document.getElementById('input');
-        input.value = "";
-    };
-    
-  
-
-//Shake it button starts animation
-btnShakeIt.addEventListener("click", () => {
-    circle.classList.add("shakeit");
-    smCircle.classList.add("shakeit");
-    mdCircle.classList.add("shakeit");
-    triangle.classList.add("shakeit");
-    triangle.classList.add("fade");
-    tbox.classList.add("shakeit");
-    tbox.classList.add("fade"); 
-  
-
-});
-
+function clearInput() {
+  document.getElementById("input").value = "";
+}
 
 
 //shake it button removes animation - allows for 1 click reaction
 circle.addEventListener("animationend", () => {
-    circle.classList.remove("shakeit");
+  circle.classList.remove("shakeit");
 });
 smCircle.addEventListener("animationend", () => {
-    smCircle.classList.remove("shakeit");
+  smCircle.classList.remove("shakeit");
 });
 mdCircle.addEventListener("animationend", () => {
-    mdCircle.classList.remove("shakeit");
+  mdCircle.classList.remove("shakeit");
+});
+outlineTriangle.addEventListener("animationend", () => {
+  outlineTriangle.classList.remove("shakeit");
+  outlineTriangle.classList.remove("fade");
 });
 triangle.addEventListener("animationend", () => {
-    triangle.classList.remove("shakeit");
-    triangle.classList.remove("fade");
+  triangle.classList.remove("shakeit");
+  triangle.classList.remove("fade");
 });
 tbox.addEventListener("animationend", () => {
-    tbox.classList.remove("shakeit");
-    tbox.classList.remove("fade");
+  tbox.classList.remove("shakeit");
+  tbox.classList.remove("fade");
 });
 
+//regex input v
 
 
+btnShakeIt.addEventListener('click', (e) => {
+
+    checkInput();
+});
+
+function checkInput() {
+  //checks char in input field
+  const inputValue = input.value.trim();
+
+if(inputValue === '') {
+  //show error
+  //add error class
+  setErrorFor(input, 'you must ask a question to receive a reading');
+  document.getElementById('tbox').innerHTML = "";
+} else {
+  setTimeout(getFromEightBallApi,2500);
+  clearInput();
+  setSuccessfor(input);
+  //Shake it button starts animation
+  circle.classList.add("shakeit");
+  smCircle.classList.add("shakeit");
+  mdCircle.classList.add("shakeit");
+  outlineTriangle.classList.add("shakeit");
+  outlineTriangle.classList.add("fade");
+  triangle.classList.add("shakeit");
+  triangle.classList.add("fade");
+  tbox.classList.add("shakeit");
+  tbox.classList.add("fade");
+}};
 
 
-
-
-/*
-async function getData() {
-try {
-const response = await fetch('https://yesno.wtf/api')
-const data = await response.json();
-console.log(data);
-showResult(data);
-} catch (err) {
-console.log('uh oh');
+function setErrorFor(input, message) {
+    const small = inputControl.querySelector('small');
+    //adds error messsage in small tag
+    small.innerText = message;
+    // adds error class
+    inputControl.className = 'input-control error';
 }
+
+
+function setSuccessfor(input) {
+  const small = inputControl.querySelector('small');
+  // adds success class
+  inputControl.className = 'input-control success';
 }
-
-*/
-
-
-
-
-
-
-
-
-
---
+++ server/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+const fortunes = [
+  "it is certain",
+  "ask again later",
+  "very doubtful",
+  "outlook good",
+  "yes",
+  "lets hope so",
+  "error 404",
+  "better not tell you now",
+  "it is decidedly so",
+  "concentrate and ask again",
+  "no"
+];
+
+app.get("/", (request, response) => {
+    const answer = fortunes[Math.floor(Math.random() * fortunes.length)];
+    response.json({body: answer});
+
+});
+
+app.listen(3000, () => {
+  console.log("Listening: http://localhost:3000/");
+});
